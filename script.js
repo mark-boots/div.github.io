56ac19da71b4e9d82aa275ad975c2213bbdaad84
Mark Boots <info@mark-boots.nl>
2023-10-13 12:40:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
-const loc = (urlParams.get('loc') || "").replace(/\s/g, " ");
+const loc = (urlParams.get('loc') || "").replace("_", " ");
 const off = Number(urlParams.get('utc') || 0);   
 const output = document.querySelector("#output");
 
@@ -8,8 +8,8 @@ function calcTime() {
     const d = new Date();
     const utc = d.getTime() + (d.getTimezoneOffset() * 60000);
     const nd = new Date(utc + (3600000 * off));
-    const timeString = nd.toLocaleString([], { hour: "2-digit", minute: "2-digit"});
-    output.innerText = (loc !== "" ? loc + ": " : "") + timeString
+    const timeString = nd.toLocaleString([], { hour: "numeric", minute: "2-digit"});
+    output.innerText = (loc !== "" ? loc + " | " : "") + timeString
     
     setTimeout(calcTime,1000)
 }
