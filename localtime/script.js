178d9620f911f82ce9c01ed58bae47d2b0ba4de9
Mark Boots <info@mark-boots.nl>
2023-10-13 12:20:06 +0200

    first commit

---
+++ localtime/script.js
@@ -0,0 +1,15 @@
+const urlParams = new URLSearchParams(window.location.search);
+const loc = urlParams.get('loc') || "";
+const off = Number(urlParams.get('utc') || 0);   
+const output = document.querySelector("#output");
+
+calcTime()
+function calcTime() {
+    const d = new Date();
+    const utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    const nd = new Date(utc + (3600000 * off));
+    const timeString = nd.toLocaleString([], { hour: "2-digit", minute: "2-digit"});
+    output.innerText = (loc !== "" ? loc + ": " : "") + timeString
+    
+    setTimeout(calcTime,1000)
+}
